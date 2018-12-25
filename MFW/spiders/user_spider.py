92e9b46ba224ade1561380c1aeb154c21debe3b2
chendansi <chendansi@guazi.com>
2018-12-25 10:27:30 +0800

    update

--- MFW/spiders/user_spider.py
+++ MFW/spiders/user_spider.py
@@ -4,6 +4,7 @@ import uuid
 import re
 import scrapy
 from MFW.user_item import UserItem
+from MFW.utils.mongo_client import connect_table
 from datetime import datetime
 
 
@@ -12,11 +13,17 @@ HOST = "http://www.mafengwo.cn"
 
 class UserSpider(scrapy.Spider):
     name = "user"
+    mongo_table = "mfw.user"
 
     start_urls = ["http://www.mafengwo.cn/"]
-    user_regex = re.compile("http://www\.mafengwo\.cn/u/\d+\.html")
+    user_regex = re.compile("(http://www\.mafengwo\.cn/u/(\d+)\.html)")
     int_list = ["level", "follow_num", "fans_num", "honey_num", "vistied_country", "visitied_mdd", "comment_num","user_id"]
 
+
+    def __init__(self, *args, **kwargs):
+        super(UserSpider, self).__init__(*args, **kwargs)
+        table = connect_table(self.mongo_table)
+        self.crawled_user_id = set(table.distinct("user_id"))
 
     def parse(self, response):
         for url in response.css('a::attr(href)').extract():
@@ -29,19 +36,23 @@ class UserSpider(scrapy.Spider):
             regex_result =  self.user_regex.findall(url)
             if regex_result:
                 uid = str(uuid.uuid4())
-                yield scrapy.Request(regex_result[0],
+                user_url, user_id = regex_result[0]
+                if int(user_id) in self.crawled_user_id:
+                    continue
+                yield scrapy.Request(user_url,
                                      cookies={
                                          "mfw_uuid": uid,
                                          "__mfwuuid": uid,
                                      },
-                                     callback=self.user_parse)
-            else:
-                yield scrapy.Request(url)
+                                     callback=self.user_parse,
+                                     meta={
+                                         "user_id": user_id
+                                     })
 
     def user_parse(self, response):
         yield from self.parse(response)
         item = UserItem()
-        item["user_id"] = response.url.split("/")[-1].split('.')[0]
+        item["user_id"] = response.meta["user_id"]
         item["name"] = response.css(".MAvaName::text").extract_first().strip()
         item["sex"] = response.css(".MAvaName i::attr(class)").extract_first()
         item["level"] = response.css(".MAvaLevel a::text").extract_first().split(".")[-1]
@@ -56,5 +67,6 @@ class UserSpider(scrapy.Spider):
                 item[k] = int(item[k])
             except:
                 item[k] = -1
+        self.crawled_user_id.add(item["user_id"])
         yield item
 
